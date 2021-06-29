acf17b320a60b896fb0bc5353e278696c2f0a92e
Brylmaz <brylmaz52@gmail.com>
2021-06-29 23:15:18 +0300

    örnek bir merhaba projesi

---
+++ stock-tracking/resources/views/index.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Laravel JS</title>
+        
+    </head>
+    <body>
+        <div id="index"></div>
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
