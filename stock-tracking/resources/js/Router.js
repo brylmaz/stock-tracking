8607e99b8954c139d471a1331c0f56e8602c312f
Brylmaz <brylmaz52@gmail.com>
2021-07-01 03:04:06 +0300

    Kullanıcı giris ve kayıt olma sayfaları yapıldı.

--- stock-tracking/resources/js/Router.js
+++ stock-tracking/resources/js/Router.js
@@ -3,15 +3,15 @@ import {Route , Switch} from 'react-router-dom';
 
 //Sayfalar
 
-import FrontIndex from './Views/Index';
-import FrontLogin from './Views/Login';
-import FrontRegister from './Views/Register';
+import IndexPage from './Views/Index';
+import LoginPage from './Views/Login';
+import RegisterPage from './Views/Register';
 
 const Main =() => (
     <Switch>
-        <Route exact path="/" component={FrontIndex}/>
-        <Route path="/login" component={FrontLogin}/>
-        <Route path="/register" component={FrontRegister}/>
+        <Route exact path="/" component={IndexPage}/>
+        <Route path="/login" component={LoginPage}/>
+        <Route path="/register" component={RegisterPage}/>
     </Switch>
 );
 
